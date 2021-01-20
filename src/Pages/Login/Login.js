957ca23a975a37659778b5b7b9e99d9f93ddc80d
Erik Longuepee <erik.longuepee@gmail.com>
2021-01-20 01:59:30 +0100

    fixed login button

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,15 @@
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Container } from 'react-bootstrap'
 import React from 'react'
 
 
 export default function Login(props) {
     return (
-        <div>
-            <a href={`${props.apiUrl}/auth/google/`}><button>Login with Google</button></a>
-        </div>
+        <Container className="my-3">
+            <Button variant="info" href={`${props.apiUrl}/auth/google/`}>
+                Login with Google <FontAwesomeIcon icon={faGoogle} />
+            </Button>
+        </Container>
     )
 }
