01ccce01a65557f40357ff75886cb9db3850899f
Erik Longuepee <erik.longuepee@gmail.com>
2021-01-21 01:12:39 +0100

    Fixed bottom social links off screen

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <Navbar bg="dark" variant="dark" className="mt-3 mt-auto">
             <Container>
                 <Navbar.Brand href="https://www.erik-longuepee.com/" target="_blank" rel="noopener noreferrer" className="ml-2"><img height="50" src="./logo_small.png" alt="logo" /> A project by Erik Longuepee</Navbar.Brand>
-                <Nav >
+                <Nav className="d-flex flex-wrap">
                     <Nav.Link href="https://github.com/ecron11" target="_blank" rel="noopener noreferrer">Github <FontAwesomeIcon icon={faGithub} /></Nav.Link>
                     <Nav.Link href="https://www.linkedin.com/in/erik-longuepee/" target="_blank" rel="noopener noreferrer">LinkedIn <FontAwesomeIcon icon={faLinkedin} /></Nav.Link>
                     <Nav.Link href="https://twitter.com/ErikLonguepee" target="_blank" rel="noopener noreferrer">Twitter <FontAwesomeIcon icon={faTwitter} /></Nav.Link>
